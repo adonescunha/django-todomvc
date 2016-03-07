c0e6a024ff743d16b24856f96066bebd8d655099
Adones Cunha <adonescunha@gmail.com>
2016-03-07 18:25:00 -0300

    Task model

--- todomvc/models.py
+++ todomvc/models.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
