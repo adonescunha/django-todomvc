3e66ce44903f17ac1c5fcd1474a52c790066138a
Adones Cunha <adonescunha@gmail.com>
2016-03-14 08:02:08 -0300

    Fix download_url configuration

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ from setuptools import setup, find_packages
 
 from todomvc import version
 
+VERSION = version.to_str()
+
 
 setup(
     name='django-todomvc',
-    version=version.to_str(),
+    version=VERSION,
     description='TodoMVC django app',
     author='Adones Cunha',
     author_email='adonescunha@gmail.com',
     url='https://github.com/adonescunha/django-todomvc',
-    download_url='https://github.com/adonescunha/django-todomvc/tarball/1.0.0',
+    download_url='https://github.com/adonescunha/django-todomvc/archive/v{0}.tar.gz'.format(VERSION),  # noqa
     packages=find_packages(exclude=['tests']),
     package_data={
         'todomvc': [
